7c07b2dd5424863b51e9f2db20ed369929e7a7b7
Sense <105939929+0xSense@users.noreply.github.com>
2023-12-14 17:09:21 -0500

    feat: sprite movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,15 +1,36 @@
 using Godot;
 using System;
 
-public partial class Player : Sprite2D
+public partial class Player : CharacterBody2D
 {
-    public override void _Ready()
-    {
+	[Export]
+	public int Speed { get; set; } = 400;
 
-    }
+	private Vector2 _target;
 
-    public override void _Process(double delta)
-    {
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("click"))
+		{
+			_target = GetGlobalMousePosition();
+		}
+	}
 
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		//pc user controls & mobile controls
+		// Physics based movement than gets confirmed on chain and changes the dirrection the sprite moves too
+
+		Velocity = Position.DirectionTo(_target) * Speed;
+
+
+		if (Position.DistanceTo(_target) > 10)
+		{
+			MoveAndSlide();
+		}
+
+	}
+
 }
